63a34cddd1996fa89578522c2c9ec6d5f29f6309
mcferrenm <mcferrenmax@gmail.com>
2019-05-31 11:34:17 -0400

    printing names and files sizes, no user args yet

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,41 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
+  // Initialize variables
+  DIR *dir;
+  struct dirent *dp;
+  struct stat buf;
+
   // Parse command line
 
   // Open directory
+  if ((dir = opendir(".")) == NULL) {
+    perror ("Cannot open .");
+    exit (1);
+  }
 
   // Repeatly read and print entries
 
+  while ((dp = readdir(dir)) != NULL) {
+    // print out file sizes
+    stat(dp->d_name, &buf);
+    printf("%10lld  ", buf.st_size);
+
+    // print out file names
+    printf("%s\n", dp->d_name);
+  }
+  
+
   // Close directory
+  closedir(dir);
 
   return 0;
 }
